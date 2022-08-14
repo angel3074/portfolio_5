e865f44c8522b7da1be3e2cd5fa8a69f44ffbaaf
angel3074 <angelluis3074@gmail.com>
2022-08-14 18:26:35 -0400

    commit

--- js.js
+++ js.js
@@ -4,3 +4,14 @@ const show = document.querySelector('#menu')
 btn.addEventListener('click', () => {
  console.log(show.classList.toggle('show_menu')); 
 })
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
