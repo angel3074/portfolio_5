8d53d485559cdf71943b3c67a440129a2a100223
angelluis3074@gmail.com <angelluis3074@gmail.com>
2022-08-23 12:28:23 -0400

    Personalize

--- js.js
+++ js.js
@@ -16,3 +16,20 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
+// const ovejas = [
+//   { name: 'Noa', color: 'azul' },
+//   { name: 'Euge', color: 'rojo' },
+//   { name: 'Navidad', color: 'rojo' },
+//   { name: 'Ki Na Ma', color: 'rojo'},
+//   { name: 'AAAAAaaaaa', color: 'rojo' },
+//   { name: 'Nnnnnnnn', color: 'rojo'}
+// ]
+
+// function count (myshit){
+//   let mapping = myshit.map((task) => task.name);
+//   return mapping;
+// }
+
+ 
+// console.log(count(ovejas));
+
